8338abd546f010405c3aa2e3985948129334fdf3
Matthias Bock <mail@matthiasbock.net>
2021-01-05 18:12:36 +0000

    buildenv: Install relevenat packages and .bashrc

--- buildenv/build-buildenv.sh
+++ buildenv/build-buildenv.sh
@@ -4,10 +4,18 @@
 docker create -it --name=buildenv -v ~/src:/usr/local/src debian:buster
 
 docker start buildenv
+
 docker exec -it buildenv bash -c "apt update && apt -y install ca-certificates apt-transport-https"
 docker cp ../sources.list.d/buster.list buildenv:/etc/apt/sources.list
-docker exec -it buildenv bash -c "apt update && apt -y install gcc g++ binutils automake autoconf make ninja-build gcc-arm-none-eabi libnewlib-nano-arm-none-eabi"
-docker stop buildenv
+docker exec -it buildenv bash -c "apt update"
 
+pkgs=$(echo $(cat ../packages/*.list))
+docker exec -it buildenv bash -c "apt -y install $pkgs"
+
+cat ../shell/*.bashrc > .bashrc
+docker cp .bashrc buildenv:/root/
+rm .bashrc
+
+docker stop buildenv
 docker container ls -a
 
