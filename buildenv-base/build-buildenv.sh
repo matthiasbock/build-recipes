dd1e9149f58523d7a5d8ce5b07f0502783a34258
Matthias Bock <mail@matthiasbock.net>
2021-01-25 21:45:47 +0000

    Enhanced script to build buildenv-base

--- buildenv-base/build-buildenv.sh
+++ buildenv-base/build-buildenv.sh
@@ -1,33 +1,69 @@
 #!/bin/bash
 
+base_image="debian:buster"
+container_name="buildenv_base"
+user="c3po"
+package_bundles="keyrings console-tools version-control build-tools c python3"
+
+common="../common"
+src_host="~/src"
+src_container="/usr/local/src"
+
+#image=...
+#containers=...
 volumes=$(docker volume ls | awk '{ if ($2 != "VOLUME") { print $2; } }')
 
-echo $volumes
-
-if [ $volumes ?? 'ccache' ]; then
-	echo "No volume named ccache"
+if [ "$(echo $volumes | fgrep ccache)" == "" ]; then
+	echo "No ccache volume found. Creating ..."
+	docker volume create ccache
+	echo "Created."
 else
-	echo "ccache volume found"
+	echo "Found ccache volume. Using ..."
 fi
 
-exit
+#
+# Create the container
+#
+echo "Creating container $container_name ..."
+docker create -it --name=$container_name -v $src_hots:$src_container -v ccache:/home/$user/.ccache $base_image
 
-# The projects to be compiled are expected in ~/src
-docker create -it --name=buildenv -v ~/src:/usr/local/src debian:buster
+docker start $container_name
 
-docker start buildenv
+#
+# Configure bash
+#
+echo "Configuring bash ..."
+tmpfile=".bashrc"
+cat $common/shell/*.bashrc > $tmpfile
+docker cp $tmpfile $container_name:/root/
+docker cp $tmpfile $container_name:/home/$user/
+rm $tmpfile
 
-docker exec -it buildenv bash -c "apt update && apt -y install ca-certificates apt-transport-https"
-docker cp ../sources.list.d/buster.list buildenv:/etc/apt/sources.list
-docker exec -it buildenv bash -c "apt update"
+#
+# Enable package installation via APT
+#
+echo "Enabling package installation ..."
+docker exec -it $container_name bash -c "apt update && apt install -y ca-certificates apt-transport-https"
+docker cp $common/sources.list.d/buster.list $container_name:/etc/apt/sources.list
+docker exec -it $container_name bash -c "apt update"
 
-pkgs=$(cat ../packages/*.list ../packages/build-dep/*.list | sed -e "s/\n\n/\n/g")
-docker exec -it buildenv bash -c "apt -y --ignore-missing install $pkgs"
+#
+# Install additional packages
+#
+pkgs=""
+for bundle in $package_bundles; do
+	echo "Adding package bundle: \"$bundle\""
+	pkgs="$pkgs $(cat $common/package-bundles/$bundle.list)"
+done
+pkgs=$(echo -n $pkgs | sed -e "s/  / /g")
+#echo $pkgs
+echo "Installing $(echo -n $pkgs | wc -w) additional packages ..."
+if [ "$pkgs" != "" ]; then
+	docker exec -it $container_name apt install -y --ignore-missing $pkgs
+fi
 
-cat ../shell/*.bashrc > .bashrc
-docker cp .bashrc buildenv:/root/
-rm .bashrc
-
-docker stop buildenv
+# Done.
+echo "Successfully created container $container_name."
+docker stop $container_name
 docker container ls -a
 
