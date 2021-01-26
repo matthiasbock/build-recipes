b958c6a2654a19fbabdbce313b0a881f65c66caf
Matthias Bock <mail@matthiasbock.net>
2021-01-26 16:40:28 +0000

    Added functions to create containers and volumes

--- apt-cache/build.sh
+++ apt-cache/build.sh
@@ -6,24 +6,16 @@ cd $(dirname $0)
 source ../common/docker.sh
 source conf.sh
 
-if [ "$(echo $volumes | fgrep $apt_cache_volume)" == "" ]; then
-	echo -n "Creating new volume '$apt_cache_volume' ... "
-	docker volume create $apt_cache_volume &> /dev/null
-	echo "Done."
-else
-	echo "Volume '$apt_cache_volume' already exists. Skipping."
-fi
+create_volume $apt_cache_volume
 
-if [ "$(echo $containers | fgrep $apt_cache_container)" == "" ]; then
-	echo -n "Creating new container '$apt_cache_container' ... "
+function constructor()
+{
 	docker run --detach \
 		--name $apt_cache_container \
 		--net $net \
 		--network-alias $apt_cache_container \
 		-p 3142:3142 \
 		-v $apt_cache_volume:/var/cache/apt-cacher-ng mbentley/apt-cacher-ng &> /dev/null
-	echo "Done."
-else
-	echo "Container '$apt_cache_container' already exists. Skipping."
-fi
+}
+create_container $apt_cache_container constructor
 
--- common/docker.sh
+++ common/docker.sh
@@ -2,11 +2,17 @@
 
 set -e
 
+#
+# docker interface
+#
 export images=$(docker image ls -a --format "{{.Repository}}:{{.Tag}}")
 export containers=$(docker container ls -a --format "{{.Names}}" | awk '{ print $1 }')
 export volumes=$(docker volume ls --format "{{.Name}}")
 export networks=$(docker network ls --format "{{.Name}}")
 
+#
+# Make sure, a shared docker network exists
+#
 export net="buildenv"
 
 if [ "$(echo $networks | fgrep $net)" == "" ]; then
@@ -14,4 +20,33 @@ if [ "$(echo $networks | fgrep $net)" == "" ]; then
 	docker network create $net &> /dev/null
 	echo "Done."
 fi
+
+#
+# Common functions
+#
+function create_volume()
+{
+	volume="$1"
+	echo -n "Creating new volume '$volume' ... "
+	if [ "$(echo $volumes | fgrep $volume)" == "" ]; then
+		docker volume create $volume &> /dev/null
+		echo "Done."
+	else
+		echo "already exists. Skipping."
+	fi
+}
+
+function create_container()
+{
+	container="$1"
+	# Containers may be constructed differently. Using the referenced constructor.
+	constructor="$2"
+	echo -n "Creating new container '$container' ... "
+	if [ "$(echo $containers | fgrep $container)" == "" ]; then
+		$constructor
+		echo "Done."
+	else
+		echo "already exists. Skipping."
+	fi
+}
 
