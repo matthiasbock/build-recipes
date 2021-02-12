ec368acf9bdc4df02c4ab359bc389b3807b91114
Matthias Bock <mail@matthiasbock.net>
2021-02-12 00:53:06 +0000

    Building apt-cache container with podman

--- apt-cache/build.sh
+++ apt-cache/build.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-set -e
 #set -x
+
+set -e
 cd $(dirname $0)
 source ../common/container.sh
 source conf.sh
+set +e
 
 create_volume $apt_cache_volume
 
 function constructor()
 {
-	docker run --detach \
+	$cli run --detach \
 		--name $apt_cache_container \
-		--net $net \
-		--network-alias $apt_cache_container \
-		-p 3142:3142 \
+		--pod $pod \
 		-v $apt_cache_volume:/var/cache/apt-cacher-ng mbentley/apt-cacher-ng &> /dev/null
+
+#		--net $net \
+#		--network-alias $apt_cache_container \
+#		-p 3142:3142 \
 }
 create_container $apt_cache_container constructor
 
--- common/container.sh
+++ common/container.sh
@@ -5,11 +5,12 @@
 #
 set -e
 
-export cli="docker"
-#export cli="podman"
+#export cli="docker"
+export cli="podman"
 
 # The shared network between the containers
 export net="buildenv"
+export pod="buildenv"
 
 #
 # Query existing container stuff
