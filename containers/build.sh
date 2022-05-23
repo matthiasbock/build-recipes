ad5586bc5d4231a17475540254cbe1a082b1dc93
Matthias Bock <mail@matthiasbock.net>
2022-05-23 14:42:30 +0200

    Defaulting to not skipping container creation and start

--- containers/build.sh
+++ containers/build.sh
@@ -21,29 +21,39 @@ test -f "${scriptpath}" \
 cd $(dirname $(realpath "$scriptpath")) \
  || { echo "Error: Failed to change to working directory. Aborting."; exit 1; }
 
+# Default: don't skip; can be enabled in the setup script (below)
+export skip_container_creation=false
+export skip_container_start=false
+
 # Prepare for script execution
 export container_config="$1"
 source "setup.sh" \
  || { echo "Error: Failed to load setup script. Aborting."; exit 1; }
 
-
 #
 # Create the container
 #
-if container_exists "$container_name"; then
-	echo "Warning: Container '$container_name' already exists. Aborting."
-	exit 0
+if $skip_container_creation; then
+  echo "Skipping container creation as requested."
+else
+  if container_exists "$container_name"; then
+  	echo "Warning: Container '$container_name' already exists. Aborting."
+  	exit 0
+  fi
+  create_container "$container_name" $container_constructor \
+   || { echo "Error: Failed to create container. Aborting. "; exit 1; }
 fi
-
-create_container "$container_name" $container_constructor \
- || { echo "Error: Failed to create container. Aborting. "; exit 1; }
 
 #
 # Work on the newly created container
 #
-echo "Starting container ..."
-container_start "$container_name" \
- || { echo "Error: Unable to start newly created container. Aborting."; exit 1; }
+if $skip_container_start; then
+  echo "Skipping container start as requested."
+else
+  echo "Starting container ..."
+  container_start "$container_name" \
+   || { echo "Error: Unable to start newly created container. Aborting."; exit 1; }
+fi
 
 # Run setup function from configuration script
 container_setup
